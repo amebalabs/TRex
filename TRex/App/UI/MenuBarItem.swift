461b94c97850cfb5507aa93b23e35d5966f3809a
Alex Mazanov <alexandr.mazanov@gmail.com>
2021-06-05 11:22:41 -0400

    Fixing capitalization

--- TRex/App/UI/MenuBarItem.swift
+++ TRex/App/UI/MenuBarItem.swift
@@ -8,7 +8,7 @@ class MenubarItem: NSObject {
     let preferences = Preferences.shared
     var statusBarItem: NSStatusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
     let statusBarmenu = NSMenu()
-    let captureTextItem = NSMenuItem(title: "Capture text", action: #selector(captureText), keyEquivalent: "")
+    let captureTextItem = NSMenuItem(title: "Capture Text", action: #selector(captureText), keyEquivalent: "")
     let ignoreLineBreaksItem = NSMenuItem(title: "Ignore Line Breaks", action: #selector(ignoreLineBreaks), keyEquivalent: "")
     let preferencesItem = NSMenuItem(title: "Preferences...", action: #selector(showPreferences), keyEquivalent: "")
     let quitItem = NSMenuItem(title: "Quit", action: #selector(quit), keyEquivalent: "q")
