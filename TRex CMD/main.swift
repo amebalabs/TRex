24ff1df13af5171e75cc6379cbd528f4388032a4
Alex Mazanov <alexandr.mazanov@gmail.com>
2022-02-06 07:49:31 -0800

    Add automation and clipboard supprot to commandline

--- TRex CMD/main.swift
+++ TRex CMD/main.swift
@@ -4,8 +4,18 @@ import Foundation
 let trex = TRex()
 
 struct TRexCMD: ParsableCommand {
+    @Flag(name: .shortAndLong, help: "Run Automation")
+    var automation = false
+
+    @Flag(name: .shortAndLong, help: "Capture from Clipboard")
+    var clipboard = false
+
     mutating func run() throws {
-        trex.capture(.captureScreen)
+        if clipboard {
+            trex.capture(automation ? .captureClipboardAndTriggerAutomation : .captureClipboard)
+            return
+        }
+        trex.capture(automation ? .captureScreenAndTriggerAutomation : .captureScreen)
     }
 }
 
