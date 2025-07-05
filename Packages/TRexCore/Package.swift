7a8683efeee1597d5fc8e0bbe19a5e45b21b8fe7
Alex Mazanov <alexandr.mazanov@gmail.com>
2025-07-04 20:06:58 -0400

    Switch to public package
    Signed-off-by: Alex Mazanov <alexandr.mazanov@gmail.com>

--- Packages/TRexCore/Package.swift
+++ Packages/TRexCore/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(path: "../../../TesseractSwift")
+        .package(url: "https://github.com/amebalabs/TesseractSwift.git", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
