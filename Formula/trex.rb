b09b04c30175d644f5ee23aaa5e3ec0de3e1d02e
GitHub Action <action@github.com>
2026-02-13 18:38:23 +0000

    docs: update CHANGELOG.md, appcast and Homebrew formula for v2.0.0

--- Formula/trex.rb
+++ Formula/trex.rb
@@ -1,6 +1,6 @@
 cask "trex" do
   version "2.0.0"
-  sha256 "c4b5a0ff9895cb1317fe70d469cc3b37b7dc9e276f4f6b399ba46c5961ba5303"
+  sha256 "4e8defc680daa6e09cb2daba576aa243683c775b93a9510ce5f12544d222de4d"
 
   url "https://github.com/amebalabs/TRex/releases/download/v#{version}/TRex-#{version}.zip"
   name "TRex"
